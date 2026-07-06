25c9f258f513d54a6e480c4889bd96cf689b005c
rohit mor <morrohit110@gmail.com>
2026-07-06 17:07:40 +0530

    this is escape sequence

---
+++ escape sequence.py
@@ -0,0 +1,51 @@
+a = ("hello rohit\nhow are you")
+
+print(a) 
+# its return "hello rohit
+# how are you" because the \n is used to create a new line in the string
+
+
+#sigle quote is used to create a string with single quotes
+a = ("himanshu is a \'good\'boy")
+print(a)
+
+#double quote is used to create a string with double quotes
+a = ("himanshu is a \"good\" boy")
+print(a)
+
+#backslash is used to create a string with backslash
+a = ("himanshu is a \\good\\ boy")
+print(a)
+
+#horizontal tab is used to create a string with horizontal tab
+a = ("himanshu is bad boy \tbut he is a good boy")
+
+print(a)
+#is used to create a space between the words in the string
+
+#backspace is used to create a string with backspace
+a = ("himanshu is a bad boy\b but he is a good\b boy")
+
+#backspace is used to remove the character before the backspace
+print(a)
+
+#formatted string is used to create a string with formatted values
+
+a = ("himanshu is a good boy and he is {} years old".format(20))
+
+#format function is used to format the string with the values passed in the format function
+
+print(a)
+
+
+#vertical tab 
+a = ("hima\vnshu")
+print(a)
+
+
+#null character
+a = ("hima\nnshu")
+print(a)
+
+
+
