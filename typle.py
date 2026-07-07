226f65fc586fe83bbc99b0593c081e8853fe12f0
rohit mor <morrohit110@gmail.com>
2026-07-07 16:15:31 +0530

    this is tuples file

---
+++ typle.py
@@ -0,0 +1,22 @@
+a = (2, 3, 5, 6, 3, "rohit", "mohit")
+
+print(type(a))
+
+
+#typle method+++++++++++++++++++
+
+#count method================
+a = (2, 3, 5, 6, "rohit", "mohit")
+
+print(type(a))
+no = a.count(3)
+print(no)
+
+#index method=================
+a = (2, 4, 3, 5, 6, "rohit", "mohit")
+
+print(type(a))
+
+i = a.index(3)
+
+print(i)
